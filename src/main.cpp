458dc4bd844fc0c283c8b04f5022fdc0e121a30f
Hazel Viswanath <viswanath.hazel@gmail.com>
2025-09-23 17:14:02 -0600

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,12 @@ int main() {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    // Uncomment this block to pass the first stage
-    std::cout << "$ ";
+    while (true) {
+        std::cout << "$ ";
 
-    std::string input;
-    std::getline(std::cin, input);
+        std::string input;
+        std::getline(std::cin, input);
 
-    std::cout << input << ": command not found" << std::endl;
+        std::cout << input << ": command not found" << std::endl;
+    }
 }
