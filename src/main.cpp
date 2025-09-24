06d3d52dcbbcc9594dcf1334021b4e1f5e24952c
Hazel Viswanath <viswanath.hazel@gmail.com>
2025-09-23 18:10:50 -0600

    string splitting

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <string>
+
+std::vector<std::string> split(std::string input) {
+    std::vector<std::string> tokens;
+    
+    for (int i = 0; i < input.length(); i++) {
+        std::string token = "";
+        for (char ch : input.substr(i)) {
+            if (ch == ' ') { break; }
+            else {
+                token += ch;
+                i++;
+            }
+        }
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
 
 int main() {
     // Flush after every std::cout / std:cerr
@@ -11,7 +31,14 @@ int main() {
 
         std::string input;
         std::getline(std::cin, input);
-
-        std::cout << input << ": command not found" << std::endl;
+        
+        std::vector<std::string> inputs = split(input);
+        if (inputs[0] == "exit") {
+            std::string arg = inputs[1];
+            exit(stoi(arg));
+        }
+        else {
+            std::cout << inputs[0] << ": command not found" << std::endl;
+        }
     }
 }
